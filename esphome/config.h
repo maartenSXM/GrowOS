940b652fe2b1f9d0357f0a9bbaa011f23f74c861
Maarten Koning <maarten@koning.ca>
2023-12-22 12:57:27 +0000

    Added cpu1.h pin definitions

--- esphome/config.h
+++ esphome/config.h
@@ -10,14 +10,20 @@
 #define GB_CONFIG_NET_WEB_SERVER	1
 
 // cpu0-specific config
+
 #if _CPU == 0
+
 #define GB_CONFIG_APP0_LED		1
 #define GB_CONFIG_CPU1_DEFAULTS_ON	0
 #define GB_CONFIG_TFT_PWR_DEFAULTS_ON 	0
 #define GB_CONFIG_TFT_BACKLIGHT_DEFAULTS_ON 1
+#define GB_CONFIG_BUZZER 		0
+#define GB_CONFIG_BUZZER_MONOTONE	0
+
 #endif // _CPU == 0
  
 // cpu1-specific config
+
 #if _CPU == 1
 
 #endif // _CPU == 1
