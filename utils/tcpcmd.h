28174a63e748125b7beeb0f5cf633af2dd923230
Maarten Koning <maarten@koning.ca>
2024-09-04 10:50:49 -0400

    new remote shell

---
+++ utils/tcpcmd.h
@@ -0,0 +1,2 @@
+extern "C" {esp_err_t tcpcmdServerInit(void); }
+extern "C" {esp_err_t tcpcmdServerTerm(void); }
--- utils/telnetd.h
+++ utils/telnetd.h
@@ -1,1 +1,2 @@
 extern "C" {esp_err_t telnetServerInit(void); }
+extern "C" {esp_err_t telnetServerTerm(void); }
