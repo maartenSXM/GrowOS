489f4c31265df1cea825b0d7ea13e34e9c8ddb99
Maarten Koning <maarten@koning.ca>
2024-09-11 15:21:29 -0400

    Added MIT license and simpleDemo header

---
+++ apps/simpleDemo/simpleDemo.h
@@ -0,0 +1,17 @@
+#ifdef GOS_BSP_lilygot4s3
+  #define WIDTH	    600
+  #define HEIGHT    450
+  #define RESIZE    100
+  #define ROTATION  270
+  #define FONT_SIZE  32
+  #define RIGHT     599 // 600 x 450
+  #define BOTTOM    449 // 600 x 450
+#else
+  #define WIDTH	    480
+  #define HEIGHT    320
+  #define RESIZE     75
+  #define ROTATION    0
+  #define FONT_SIZE  24
+  #define RIGHT     479	// 480 x 320
+  #define BOTTOM    319	// 480 x 320
+#endif
